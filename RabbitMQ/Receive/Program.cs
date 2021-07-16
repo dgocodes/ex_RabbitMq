14466171af25dc7eb13281f74b74c02e6d90eed0
sisdperillo <sis.dperillo@gmail.com>
2021-07-16 17:02:01 -0300

    Novo projeto

---
+++ RabbitMQ/Receive/Program.cs
@@ -0,0 +1,80 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region "Consumidor de mensagem simples"
+
+            //var factory = new ConnectionFactory() { HostName = "localhost" };
+            //using (var connection = factory.CreateConnection())
+            //using (var channel = connection.CreateModel())
+            //{
+            //    channel.QueueDeclare(queue: "hello",
+            //                         durable: false,
+            //                         exclusive: false,
+            //                         autoDelete: false,
+            //                         arguments: null);
+
+            //    var consumer = new EventingBasicConsumer(channel);
+            //    consumer.Received += (model, ea) =>
+            //    {
+            //        var body = ea.Body.ToArray();
+            //        var message = Encoding.UTF8.GetString(body);
+            //        Console.WriteLine(" [x] Received {0}", message);
+            //    };
+            //    channel.BasicConsume(queue: "hello",
+            //                         autoAck: true,
+            //                         consumer: consumer);
+
+            //    Console.WriteLine(" Press [enter] to exit.");
+            //    Console.ReadLine();
+            //} 
+
+            #endregion
+
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                //Indicando que somente uma mensagem sera passada ao canal
+                //Se ele ja tiver ocupado, a mensagem sera enviada pra outro receive.
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false); 
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    int dots = message.Split('.').Length -1;
+                    Thread.Sleep(dots * 1000);
+
+                    //marcando com concluida no mensageiro
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ RabbitMQ/Send/Program.cs
@@ -0,0 +1,71 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region "Fila com mensagem"
+
+            //var factory = new ConnectionFactory() { HostName = "localhost" };
+            //using (var connection = factory.CreateConnection())
+            //using (var channel = connection.CreateModel())
+            //{
+            //    channel.QueueDeclare(queue: "hello",
+            //                         durable: false,
+            //                         exclusive: false,
+            //                         autoDelete: false,
+            //                         arguments: null);
+
+            //    string message = "Hello World!";
+            //    var body = Encoding.UTF8.GetBytes(message);
+
+            //    channel.BasicPublish(exchange: "",
+            //                         routingKey: "hello",
+            //                         basicProperties: null,
+            //                         body: body);
+            //    Console.WriteLine(" [x] Sent {0}", message);
+            //}
+
+            //Console.WriteLine(" Press [enter] to exit.");
+            //Console.ReadLine();
+
+            #endregion
+
+            //var factory = new ConnectionFactory() { HostName = "localhost" };
+            //using (var connection = factory.CreateConnection())
+            //using (var channel = connection.CreateModel())
+            //{
+            //    channel.QueueDeclare(queue: "task_queue",
+            //                         durable: true, //definindo que a msg nao sera excluida da fila caso o servico pare
+            //                         exclusive: false,
+            //                         autoDelete: false,
+            //                         arguments: null);
+
+            //    var message = GetMessage(args);
+            //    var body = Encoding.UTF8.GetBytes(message);
+
+            //    var properties = channel.CreateBasicProperties();
+            //    properties.Persistent = true; //marcando as mensagns como persistentes
+
+            //    channel.BasicPublish(exchange: "",
+            //                         routingKey: "task_queue",
+            //                         basicProperties: properties,
+            //                         body: body);
+            //    Console.WriteLine(" [x] Sent {0}", message);
+            //}
+        
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
