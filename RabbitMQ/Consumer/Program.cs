f6f7c7e4d80d1b13dfa47b31fbdb985f0cc3309f
sisdperillo <sis.dperillo@gmail.com>
2021-07-16 16:47:40 -0300

    Adicionando projeto

---
+++ RabbitMQ/Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Exemplo", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(message);
+                    };
+
+                    channel.BasicConsume(queue: "Exemplo", autoAck: true, consumer: consumer);
+
+
+                    Console.WriteLine("Consumer funcionando!!!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
